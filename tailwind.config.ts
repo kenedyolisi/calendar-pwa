762f2ecb560dc6fc2144f73820a1f969f6c8dbb0
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-06 21:12:51 +0300

    chore: add tailwind configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+import { Config } from "tailwindcss";
+export default {
+  content: ["./src/**/*.{html,ts,tsx}"],
+  darkMode: ["class"],
+  theme: { extend: {} },
+  plugins: [],
+} satisfies Config;
