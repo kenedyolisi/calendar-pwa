f78c19714c53e0e99e75386f6be12b6e172b8268
Kenedy Olisi <66219869+kenedyolisi@users.noreply.github.com>
2024-01-27 23:32:09 +0300

    feat: add jumping to previous and next years

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,5 +1,6 @@
 import {
   addMonths,
+  addYears,
   eachDayOfInterval,
   endOfMonth,
   format,
@@ -7,6 +8,7 @@ import {
   isToday,
   startOfMonth,
   subMonths,
+  subYears,
 } from "date-fns";
 import { useState } from "react";
 
@@ -37,7 +39,7 @@ export const Calendar = () => {
 
             return (
               <td
-                className={`${
+                className={`text-lg ${
                   date && isToday(date) ? "bg-blue-500 text-white " : ""
                 }`}
                 key={index}
@@ -51,12 +53,20 @@ export const Calendar = () => {
     );
   }
 
-  function handlePrevClick() {
+  function handlePrevYearClick() {
+    setDate(subYears(date, 1));
+  }
+
+  function handlePrevMonthClick() {
     setDate(subMonths(date, 1));
   }
 
-  function handleNextClick() {
+  function handleNextMonthClick() {
     setDate(addMonths(date, 1));
+  }
+
+  function handleNextYearClick() {
+    setDate(addYears(date, 1));
   }
 
   function handleTodayClick() {
@@ -64,63 +74,109 @@ export const Calendar = () => {
   }
 
   return (
-    <div className="my-auto">
-      <div className="flex mb-2">
-        <button
-          className="border me-1 p-1 active:scale-90 hover:bg-gray-100"
-          type="button"
-          title="Go to previous month"
-          onClick={handlePrevClick}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            width="16"
-            height="16"
+    <div>
+      <div className="flex justify-between items-center mb-2">
+        <div className="flex gap-2">
+          <button
+            className="p-2 border hover:bg-sky-400 active:scale-95"
+            type="button"
+            title="Go to previous year"
+            onClick={handlePrevYearClick}
           >
-            <path
-              fillRule="evenodd"
-              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
-            />
-          </svg>
-        </button>
-        <button
-          className="border me-1 p-1 active:scale-90"
-          type="button"
-          title="Go to next month"
-          onClick={handleNextClick}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            width="16"
-            height="16"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              width="20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
+          </button>
+          <button
+            className="p-2 border hover:bg-sky-400 active:scale-95"
+            type="button"
+            title="Go to previous month"
+            onClick={handlePrevMonthClick}
           >
-            <path
-              fillRule="evenodd"
-              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-            />
-          </svg>
-        </button>
-        <button
-          className="border p-1 active:scale-90 hover:bg-gray-200"
-          type="button"
-          onClick={handleTodayClick}
-        >
-          Today
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              width="20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
+          </button>
+          <button
+            className="p-2 border hover:bg-sky-400 active:scale-95"
+            type="button"
+            title="Go to next month"
+            onClick={handleNextMonthClick}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              width="20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </button>
+          <button
+            className="p-2 border hover:bg-sky-400 active:scale-95"
+            type="button"
+            title="Go to next year"
+            onClick={handleNextYearClick}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              width="20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+              <path
+                fillRule="evenodd"
+                d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div>
+          <button
+            className="p-1 border text-lg hover:bg-sky-400 active:scale-95"
+            type="button"
+            onClick={handleTodayClick}
+          >
+            Today
+          </button>
+        </div>
       </div>
       <table className=" table-auto text-center">
-        <caption className="bg-blue-500 text-white">
+        <caption className="py-1 bg-blue-600 text-white text-2xl">
           {format(date, "MMMM yyyy")}
         </caption>
 
         <thead>
           <tr>
             {daysOfTheWeek.map((day) => (
-              <th className="" key={day}>
+              <th className="capitalize p-2 text-lg" key={day}>
                 {day}
               </th>
             ))}
